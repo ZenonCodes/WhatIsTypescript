491534cf2f27975bc00fabc223808acf7022dbe7
Vera E <97448213+ZenonCodes@users.noreply.github.com>
2022-04-08 19:39:58 -0400

    Putting together files

--- source/index.ts
+++ source/index.ts
@@ -1,6 +1,15 @@
-export module SmokeTest
-{
-    export function HelloWorld(){
-        console.log("Hello Shawty")
-    }
-}
+// Import stylesheets
+import './style.css';
+
+
+const form: HTMLFormElement = document.querySelector('#defineform');
+
+
+form.onsubmit = () => {
+    const formData = new FormData(form);
+
+    console.log(formData);
+    const text = formData.get('defineword') as string;
+    console.log(text);
+    return false; // prevent reload
+};
