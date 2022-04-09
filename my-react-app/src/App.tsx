2603779345c3671e0f79d6a61b8d66992be24adb
Vera E <97448213+ZenonCodes@users.noreply.github.com>
2022-04-09 01:30:55 -0400

    While its all working - not gonna tempt fate

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -27,10 +27,27 @@ form.onsubmit = () => {
 };
     return (
         <div className="App">
-            {/*<header className="App-header">*/}
-            {/*    <img src={logo} className="App-logo" alt="logo" />*/}
-            {/*    <h1>Typescript Dictionary</h1>*/}
-            {/*</header>*/}
+            <main className="container">
+                <div className="bg-light p-5 rounded">
+                    <h1>Definition</h1>
+                    <p className="lead">Use this part of the page to present your results from the API call.</p>
+                    <ul className="list-unstyled">
+                        <li>This is a list. Your word could go here.</li>
+                        <li>It appears completely unstyled.</li>
+                        <li>Maybe your multiple definitions of the word are here?</li>
+                        <li>Structurally, it's still a list.</li>
+                        <li>However, this style only applies to immediate child elements.</li>
+                        <li>Nested lists: (maybe synonyms and antonyms?)
+                            <ul>
+                                <li>are unaffected by this style</li>
+                                <li>will still show a bullet</li>
+                                <li>and have appropriate left margin</li>
+                            </ul>
+                        </li>
+                        <li>This may still come in handy in some situations.</li>
+                    </ul>
+                </div>
+            </main>
         </div>
     );
 }
